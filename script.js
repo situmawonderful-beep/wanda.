b03fde2e3ffd02a8b149155ea062fb0bb518120c
situmawonderful-beep <situmawonderful@gmail.com>
2026-05-06 22:44:16 +0300

    Update script.js

--- script.js
+++ script.js
@@ -31,11 +31,12 @@ const ADMIN_EMAIL = 'situmawonderful@gmail.com';
 const ADMIN_NAME  = 'Wanda';
 
 const SERVICES = [
-  { name: 'Manicure',         icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/manicure_b_hrhetr.jpg',        desc: 'Classic, Gel & acrylic nail treatments',                        price: "500-2500" },
-  { name: 'Pedicure',         icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/pedicure_tmimzg.jpg',          desc: 'Relaxing foot care and nail grooming',                           price: "800-3000" },
-  { name: 'Wig Installation', icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664970/wig_installation_gkokcs.jpg',  desc: 'Professional wig fitting and styling',                           price: "1000-3500" },
-  { name: 'Hair Dressing',    icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/hair_dressing_xb68xg.jpg',     desc: 'Styling, braiding, and coloring to keep you looking your best',  price:  "500-3000" },
-  { name: 'Barber Shop',      icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/cornrows_hwnvtr.jpg',          desc: 'Fresh cuts, Cornrows installation, Locs installation and braids',      price: "200-3500"  },
+  { name: 'Manicure',         icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/manicure_b_hrhetr.jpg',        desc: 'Classic, Gel & acrylic nail treatments',                        price: "500 - 2500" },
+  { name: 'Pedicure',         icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/pedicure_tmimzg.jpg',          desc: 'Relaxing foot care and nail grooming',                           price: "800 - 3000" },
+  { name: 'Wig Installation', icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664970/wig_installation_gkokcs.jpg',  desc: 'Professional wig fitting and styling',                           price: "1000 - 3500" },
+  { name: 'Hair Dressing',    icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/hair_dressing_xb68xg.jpg',     desc: 'Styling, braiding, and coloring to keep you looking your best',  price:  "500 - 3000" },
+  { name: 'Barber Shop',      icon: 'https://res.cloudinary.com/dbk47jrff/image/upload/v1777664969/cornrows_hwnvtr.jpg',          desc: 'Fresh cuts, Cornrows installation, Locs installation and braids',      price: "200 - 3500"  },
+  { name: 'Full-body Massage' icon:  'https://res.cloudinary.com/dbk47jrff/image/upload/v1778066202/massage_yut61e.jpg '           desc: 'Relaxing full-body massage to relieve stress and tension'        price: "2000 - 5000"}
 ];
 
 // ============================
